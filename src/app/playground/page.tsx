a131c4dc7cc53b4d0ba8c8541f96b7ad66331f76
Samuel Hrotik <samuel_hrotik@foxitsoftware.com>
2025-03-26 13:04:55 +0100

    feat: Fixed build error

--- src/app/playground/page.tsx
+++ src/app/playground/page.tsx
@@ -4,7 +4,7 @@ import {
   ResizablePanel,
   ResizablePanelGroup,
 } from "~/components/ui/resizable";
-import React, { useEffect, useState } from "react";
+import React, { Suspense, useEffect, useState } from "react";
 import { Button } from "~/components/ui/button";
 import CodeMirror from "@uiw/react-codemirror";
 import { xml } from "@codemirror/lang-xml";
@@ -12,8 +12,23 @@ import { Textarea } from "~/components/ui/textarea";
 import { api } from "~/trpc/react";
 import tags from "~/assets/taggsDB";
 import { useSearchParams } from "next/navigation";
+import { useIsClient } from "usehooks-ts";
 
 export default function Playground() {
+  const isClient = useIsClient();
+
+  if (!isClient) return null;
+
+  return (
+    <div className="flex min-h-screen flex-col items-center">
+      <Suspense fallback="loading...">
+        <XMLViewer />
+      </Suspense>
+    </div>
+  );
+}
+
+function XMLViewer() {
   const searchParams = useSearchParams();
   const tagName = searchParams.get("tag");
   const useCaseIndex = searchParams.get("useCase");
@@ -48,42 +63,41 @@ export default function Playground() {
 
     setValidationOutput(response);
   }
+
   return (
-    <div className="flex min-h-screen flex-col items-center">
-      <main className="container flex w-full flex-1 flex-col gap-2 pt-5">
-        <section className="w-full flex-col items-center">
-          <Button variant="default" onClick={run}>
-            Run
-          </Button>
-        </section>
-        <section className="flex h-[500px] w-full flex-col items-center">
-          <ResizablePanelGroup
-            direction="horizontal"
-            className="w-full rounded-lg border border-primary md:min-w-[450px]"
-          >
-            <ResizablePanel>
-              <CodeMirror
-                value={xmlContent}
-                height="calc(100%)"
-                extensions={[xml()]}
-                onChange={onChange}
-                theme="dark"
-                className="h-full"
-              />
-            </ResizablePanel>
-            <ResizableHandle withHandle className="bg-primary" />
-            <ResizablePanel>
-              <Textarea
-                className="h-full"
-                value={validationOutput}
-                onChange={(e) => {
-                  setValidationOutput(e.target.value);
-                }}
-              />
-            </ResizablePanel>
-          </ResizablePanelGroup>
-        </section>
-      </main>
-    </div>
+    <main className="container flex w-full flex-1 flex-col gap-2 pt-5">
+      <section className="w-full flex-col items-center">
+        <Button variant="default" onClick={run}>
+          Run
+        </Button>
+      </section>
+      <section className="flex h-[500px] w-full flex-col items-center">
+        <ResizablePanelGroup
+          direction="horizontal"
+          className="w-full rounded-lg border border-primary md:min-w-[450px]"
+        >
+          <ResizablePanel>
+            <CodeMirror
+              value={xmlContent}
+              height="calc(100%)"
+              extensions={[xml()]}
+              onChange={onChange}
+              theme="dark"
+              className="h-full"
+            />
+          </ResizablePanel>
+          <ResizableHandle withHandle className="bg-primary" />
+          <ResizablePanel>
+            <Textarea
+              className="h-full"
+              value={validationOutput}
+              onChange={(e) => {
+                setValidationOutput(e.target.value);
+              }}
+            />
+          </ResizablePanel>
+        </ResizablePanelGroup>
+      </section>
+    </main>
   );
 }
